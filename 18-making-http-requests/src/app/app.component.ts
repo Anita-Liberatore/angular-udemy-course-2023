e9219519cc3ab5c47cbaa12faff82d60f4116207
Anita Liberatore <anita.liberatore@spindox.it>
2023-02-23 23:21:20 +0100

    fetchPosts function

--- 18-making-http-requests/src/app/app.component.ts
+++ 18-making-http-requests/src/app/app.component.ts
@@ -14,18 +14,31 @@ export class AppComponent {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: Post) {
     // Send Http request
     console.log(postData);
+    this.http.post('https://angular-udemy-course-d7968-default-rtdb.firebaseio.com/posts.json', 
+    postData).subscribe(response => {
+      console.log(response)
+    });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get('https://angular-udemy-course-d7968-default-rtdb.firebaseio.com/posts.json').subscribe(posts => {
+      console.log(posts)
+    })
   }
 }
