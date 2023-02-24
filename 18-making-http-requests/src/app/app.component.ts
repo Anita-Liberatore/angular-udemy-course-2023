9021bfc0666af42e835dbcdd426af99797b90e6b
Anita Liberatore <anita.liberatore@spindox.it>
2023-02-24 17:44:28 +0100

    fetchposts with pipe rxjs

--- 18-making-http-requests/src/app/app.component.ts
+++ 18-making-http-requests/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './model/post';
+import { map } from 'rxjs';
 
 
 @Component({
@@ -37,7 +38,17 @@ export class AppComponent {
   }
 
   private fetchPosts() {
-    this.http.get('https://angular-udemy-course-d7968-default-rtdb.firebaseio.com/posts.json').subscribe(posts => {
+    this.http.get('https://angular-udemy-course-d7968-default-rtdb.firebaseio.com/posts.json')
+    .pipe(map( responseData => {
+      const postsArray = [];
+      for(const key in responseData) {
+        if(responseData.hasOwnProperty(key)) {
+          postsArray.push({ ...responseData[key], id: key})
+        }
+      }
+      return postsArray;
+    })
+    ).subscribe(posts => {
       console.log(posts)
     })
   }
