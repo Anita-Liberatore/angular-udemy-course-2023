79b49c9b51c4d911dfab1e4cc5a01e40118c65a3
Anita Liberatore <anita.liberatore@spindox.it>
2023-02-25 20:24:17 +0100

    ngIf

--- 2-the-basics/app-server/src/app/servers/servers.component.ts
+++ 2-the-basics/app-server/src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created'
   serverName!:string;
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -19,6 +20,7 @@ export class ServersComponent implements OnInit {
   ngOnInit() {}
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created'
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created' + this.serverName
   }
 }
