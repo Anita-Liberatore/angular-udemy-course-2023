dc39f9c09cf02f1f7a7ce9ad1d3cec3e87f5e0ab
Anita Liberatore <anita.liberatore@spindox.it>
2023-02-25 19:53:59 +0100

    responsetype with interceptor

--- 18-making-http-requests/src/app/interceptor/auth-interceptor.ts
+++ 18-making-http-requests/src/app/interceptor/auth-interceptor.ts
@@ -1,12 +1,25 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Observable } from "rxjs";
+import {
+  HttpEvent,
+  HttpEventType,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 
 export class AuthInterceptor implements HttpInterceptor {
-
-    intercept(req: HttpRequest<any>, next: HttpHandler)  {
-        console.log('request is on it way')
-        const modifiedReq = req.clone({headers: req.headers.append('Auth', 'xyz')})
-        return next.handle(modifiedReq)
-    }
-    
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    console.log('request is on it way');
+    const modifiedReq = req.clone({
+      headers: req.headers.append('Auth', 'xyz'),
+    });
+    return next.handle(modifiedReq).pipe(tap(event => {
+        console.log(event)
+        if(event.type === HttpEventType.Response) {
+            console.log('Response ok')
+            console.log(event.body)
+        }
+        
+    }));
+  }
 }
