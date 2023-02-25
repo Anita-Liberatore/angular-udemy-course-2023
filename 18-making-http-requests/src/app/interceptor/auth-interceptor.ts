28ff6ade4b1f9f0dfe38df7564e69da753d4cf24
Anita Liberatore <anita.liberatore@spindox.it>
2023-02-25 19:49:36 +0100

    interceptor

--- 18-making-http-requests/src/app/interceptor/auth-interceptor.ts
+++ 18-making-http-requests/src/app/interceptor/auth-interceptor.ts
@@ -5,7 +5,8 @@ export class AuthInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler)  {
         console.log('request is on it way')
-        return next.handle(req)
+        const modifiedReq = req.clone({headers: req.headers.append('Auth', 'xyz')})
+        return next.handle(modifiedReq)
     }
     
 }
