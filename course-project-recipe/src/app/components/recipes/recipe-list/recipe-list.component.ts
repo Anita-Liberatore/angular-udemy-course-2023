a7b18bacf8ed6374508203446066e405b8d2e997
Anita Liberatore <anita.liberatore@spindox.it>
2023-02-26 15:38:36 +0100

    add list group for recipe

--- course-project-recipe/src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ course-project-recipe/src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Recipe 1',
+      'Recipe Description 1',
+      'https://www.kingarthurbaking.com/sites/default/files/styles/featured_image/public/2022-05/Tomato-Pie_0256.jpg?itok=c63mh-z9'
+    ),
+  ];
 
-  recipes = []
+  constructor() {}
+
+  ngOnInit(): void {}
 }
