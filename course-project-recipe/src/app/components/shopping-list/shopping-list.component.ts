a124680c6ebd4a2d37e7c06444a45955e35b7d52
Anita Liberatore <anita.liberatore@atscom.it>
2023-06-14 14:32:06 +0200

    add shoppingListService and add getIngredients()

--- course-project-recipe/src/app/components/shopping-list/shopping-list.component.ts
+++ course-project-recipe/src/app/components/shopping-list/shopping-list.component.ts
@@ -1,25 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.services';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
+  providers: [ShoppingListService]
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
 
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 6),
-    new Ingredient('Tomatoes', 10)
-  ];
+  ingredients!: Ingredient[]
 
-  constructor() {
+  constructor(private shoppingService: ShoppingListService) {
+  }
 
+  ngOnInit(): void {
+    this.ingredients = this.shoppingService.getIngredients();
   }
 
   onIngredientAdded(ingredient: Ingredient) {
-
     this.ingredients.push(ingredient)
-
   }
 }
--- course-project-recipe/src/app/components/shopping-list/shopping-list.services.ts
+++ course-project-recipe/src/app/components/shopping-list/shopping-list.services.ts
@@ -1,3 +1,13 @@
+import { Ingredient } from "src/app/shared/ingredient.model";
+
 export class ShoppingListService {
-    
+
+    private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 6),
+    new Ingredient('Tomatoes', 10)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice()
+  }
 }
